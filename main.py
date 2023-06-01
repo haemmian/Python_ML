72ff78a01b5ee2174749ed531bade7404e1fb8cf
Anthony Haemmig <anthony.haemmig@schindler.com>
2023-06-01 16:50:49 +0200

    Created first mini AI

--- main.py
+++ main.py
@@ -1,25 +1,81 @@
-#******************************************#
+# ******************************************#
 # Application:
 # Created:
-#******************************************#
-import os
-import openai
-from API_KEYS_ML import openAI_API_key
+# ******************************************#
+import keras
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-print("CONSOLE CHAT-GPT")
+# mnist = tf.keras.datasets.mnist
+#
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train, x_test = x_train / 255.0, x_test / 255.0
+#
+# model = tf.keras.models.Sequential([
+#   tf.keras.layers.Flatten(input_shape=(28, 28)),
+#   tf.keras.layers.Dense(128, activation='relu'),
+#   tf.keras.layers.Dropout(0.2),
+#   tf.keras.layers.Dense(10)
+# ])
+#
+# predictions = model(x_train[:1]).numpy()
+#
+#
+# loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+#
+#
+# model.compile(optimizer='adam',
+#               loss=loss_fn,
+#               metrics=['accuracy'])
+#
+# model.fit(x_train, y_train, epochs=5)
+#
+# print(model.evaluate(x_test, y_test, verbose=2))
 
-openai.api_key = openAI_API_key
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt= input("What is your question?\n"),
-  temperature=0,
-  max_tokens=150,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\"\"\""]
-)
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-answer: str = response["choices"][-1]["text"]
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
-print("ANSWER\n" + answer)
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i + 1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+model.fit(train_images, train_labels, epochs=10)
+
+probability_model = tf.keras.Sequential([model,
+                                         tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+
+print(predictions)
+
+# test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+#
+# print("\nTest accuracy: ", test_acc)
