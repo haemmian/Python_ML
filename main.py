3d5b7aa1f3e8d687b8a4a923e259c326f94da031
Anthony Haemmig <anthony.haemmig@schindler.com>
2023-06-01 13:53:13 +0200

    Create Picture with OpenAI

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-#******************************************#
-# Application:
-# Created:
-#******************************************#
+
 import os
 import openai
 from API_KEYS_ML import openAI_API_key
-
-print("CONSOLE CHAT-GPT")
-
 openai.api_key = openAI_API_key
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt= input("What is your question?\n"),
-  temperature=0,
-  max_tokens=150,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\"\"\""]
-)
 
-answer: str = response["choices"][-1]["text"]
 
-print("ANSWER\n" + answer)
+print(openai.Image.create(
+  prompt="Space Wallpaper with mars as a main focus",
+  n=2,
+  size="256x256"
+))
