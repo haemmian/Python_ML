4035d2826ea9db8afc7f41b372f0ccc450244da2
Anthony Haemmig <anthony.haemmig@schindler.com>
2023-06-07 11:00:44 +0200

    created and trained a model that can detect clothe types: main is the training module AI_CLOTHING-TEST_Module.py loads the module and uses it for the picture
    It does have problems with picture which are downsized to 28x28 Pixels. It assumes that most objects are bags

---
+++ AI_CLOTHING-TEST_Module.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load the trained model
+model = tf.keras.models.load_model('AI_N1.h5')
+
+
+# Preprocess the input image
+def preprocess_image(image_path):
+    image = Image.open(image_path)
+    image = image.convert("L")                  # Convert to grayscale
+    image = image.resize((28, 28))              # Resize to fixed size
+    image.show()
+    image = np.array(image)                     # Convert to numpy array
+    image = image.astype('float32') / 255.0     # Normalize pixel values to [0, 1]
+    image = np.expand_dims(image, axis=-1)      # Add channel dimension
+    return image
+
+
+# Load and preprocess the real image
+image_path = 'testImages/test-Tshirt.png'
+preprocessed_image = preprocess_image(image_path)
+
+# Reshape the preprocessed image to match the input shape expected by the model
+input_image = np.expand_dims(preprocessed_image, axis=0)
+
+# Perform prediction
+predictions = model.predict(input_image)
+
+# Get the predicted class index
+predicted_class_index = np.argmax(predictions)
+
+# Print the predicted class label
+class_labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+predicted_class_label = class_labels[predicted_class_index]
+
+# Print the predicted class label
+print('Predicted class label:', predicted_class_label)
+
--- main.py
+++ main.py
@@ -35,17 +35,16 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 # plt.figure()
 # plt.imshow(train_images[0])
 # plt.colorbar()
 # plt.grid(False)
 # plt.show()
-
-train_images = train_images / 255.0
-test_images = test_images / 255.0
 
 # plt.figure(figsize=(10, 10))
 # for i in range(25):
@@ -67,14 +66,77 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+model = tf.keras.models.load_model("AI_N1.h5")
 
-model.fit(train_images, train_labels, epochs=10)
+# model.fit(train_images, train_labels, epochs=10)                           # Train the AI Model with the function fit
+#
+# model.summary()
+#
+model.save("AI_N1.h5")
 
-probability_model = tf.keras.Sequential([model,
-                                         tf.keras.layers.Softmax()])
+
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 predictions = probability_model.predict(test_images)
 
-print(predictions)
+
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+#
+# i = 12
+# plt.figure(figsize=(6, 3))
+# plt.subplot(1, 2, 1)
+# plot_image(i, predictions[i], test_labels, test_images)
+# plt.subplot(1, 2, 2)
+# plot_value_array(i, predictions[i], test_labels)
+# plt.show()
+# probability_model = tf.keras.Sequential([model,tf.keras.layers.Softmax()])
+#
+# predictions = probability_model.predict(test_images)
 
 # test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 #
